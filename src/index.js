6c0b6de05a062cb271a89e82e900cadd97984258
melkadze <25963683+melkadze@users.noreply.github.com>
2019-11-21 11:44:10 -0500

    import dictionary

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { getWordInfo, DOMStrings } from "./api";
+
+DOMStrings.input.addEventListener("keypress", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector(".nes-btn").click();
+  }
+});
+
+getWordInfo();
