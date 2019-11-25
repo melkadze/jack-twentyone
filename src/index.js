52e14d28aed4d07b430881c6a5792fac6932ec79
melkadze <25963683+melkadze@users.noreply.github.com>
2019-11-25 11:45:31 -0500

    t

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { getWordInfo, DOMStrings } from "./api";
+/*import { getWordInfo, DOMStrings } from "./api";
 
 DOMStrings.input.addEventListener("keypress", function(event) {
   if (event.keyCode === 13) {
@@ -8,3 +8,29 @@ DOMStrings.input.addEventListener("keypress", function(event) {
 });
 
 getWordInfo();
+*/
+
+let deck = [];
+
+function addToDeck(suit) {
+  for (let i = 1; i < 14; i++) {
+    if (i === 1) {
+      deck.push(['A', suit])
+    } else if (i === 11) {
+      deck.push(['J', suit])
+    } else if (i === 12) {
+      deck.push(['Q', suit])
+    } else if (i === 13) {
+      deck.push(['K', suit])
+    } else {
+      deck.push([`${i}`, suit])
+    }
+  }
+}
+
+addToDeck('S');
+console.log(deck)
+
+function createDeck() {
+
+}
